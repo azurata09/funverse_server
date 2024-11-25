084ddf43590445d1dd556625757b08881a2fab5a
髙橋玄 <b1023138@fun.ac.jp>
2024-11-25 17:54:55 +0900

    useid塔の情報をterminalに表示 dbにuserid追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+//expressおまじない
 import express from 'express'
-
 const app: express.Express = express()
 
-app.get("/", (req:express.Request, res:express.Response)=>{
-  res.send("こんにちは");
+//
+app.get("/user", (req:express.Request, res:express.Response)=>{
+  res.json({
+    "name": "azurata",
+    "grade": "4",
+    "bio": "あああああああああああああああああああああああ",
+    "sns": {
+      "x": "@azurata09_",
+      "facebook": "sotaronaka09",
+      "homepage": "https://www.azurata.me/"
+    }
+  }
+)
 })
 
 app.listen(3000,()=>{
