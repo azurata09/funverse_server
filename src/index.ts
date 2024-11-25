3f60088ed60b0740f5dc527fbe194426ddf40e7b
髙橋玄 <b1023138@fun.ac.jp>
2024-11-25 17:55:25 +0900

    useid等の情報をterminalに表示 dbにuserid追加

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,57 @@
 //expressおまじない
 import express from 'express'
-const app: express.Express = express()
 
-//
-app.get("/user", (req:express.Request, res:express.Response)=>{
+import sqlite from 'sqlite3'
+import fs from 'fs'
+
+const app: express.Express = express()
+const port = 3000
+
+const initDataSql = fs.readFileSync('./sql/sampleData.sql').toString()
+const dummyDataSqls = fs.readFileSync('./sql/sampleData.sql').toString().split(';').slice(0, -1)
+
+
+const db = new sqlite.Database('./data/data.db', (err) => {
+  if(err){
+    console.error(err)
+    throw Error('データベースが開けませんでした')
+  }
+  console.log('データベースが開かれました')
+
+  db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, userID TEXT NOT NULL);', (err) => {
+    if (err) {
+      console.error(err);
+      throw Error('テーブルの作成に失敗しました')
+    }
+    console.log('テーブルが作成されました');
+  })
+})
+
+
+
+app.get("/user/:userID", (req:express.Request, res:express.Response)=>{
+  //res.send("こんにち");
   res.json({
+    "userID":req.params.userID,
     "name": "azurata",
     "grade": "4",
-    "bio": "あああああああああああああああああああああああ",
+    "bio": "あいうえお",
     "sns": {
       "x": "@azurata09_",
       "facebook": "sotaronaka09",
       "homepage": "https://www.azurata.me/"
     }
-  }
-)
+  })
+
+  db.run('INSERT INTO users (userID) Values (?);',[req.params.userID],(err) =>{
+    if(err){
+      console.error(err)
+      throw Error('データの挿入に失敗しました')
+    }
+    console.log('データが挿入されました')
+  })
 })
 
-app.listen(3000,()=>{
-  console.log('ポート3000番で起動しました。')
+app.listen(port,()=>{
+  console.log('ポート'+port+'番で起動しました。')
 })
