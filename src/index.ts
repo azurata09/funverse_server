cc0339e790157ac48af9d2cd1a0207c6ab85cb0f
azurata <git@azurata.me>
2024-12-23 12:54:14 +0900

    feat: ユーザーのビーコン接続状態を受け取るエンドポイントを作成

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 //expressおまじない
 import express from 'express'
 
-import fs from 'node:fs'
+import bodyParser from 'body-parser'
+
+// import fs from 'node:fs'
 import sqlite from 'sqlite3'
 
 const app: express.Express = express()
 const port = 3000
+app.use(bodyParser.json()) // 追加
 
-const initDataSql = fs.readFileSync('./sql/sampleData.sql').toString()
-const dummyDataSqls = fs
-  .readFileSync('./sql/sampleData.sql')
-  .toString()
-  .split(';')
-  .slice(0, -1)
+// const initDataSql = fs.readFileSync('./sql/sampleData.sql').toString()
+// const dummyDataSqls = fs
+//   .readFileSync('./sql/sampleData.sql')
+//   .toString()
+//   .split(';')
+//   .slice(0, -1)
 
 const db = new sqlite.Database('./data/data.db', (err) => {
   if (err) {
@@ -30,6 +33,10 @@ const db = new sqlite.Database('./data/data.db', (err) => {
       }
       console.log('テーブルが作成されました')
     },
+  )
+
+  db.run(
+    'CREATE TABLE IF NOT EXISTS close_table (userId TEXT PRIMARY KEY, close INTEGER NOT NULL);',
   )
 })
 
@@ -58,6 +65,44 @@ app.get('/user/:userID', (req: express.Request, res: express.Response) => {
       console.log('データが挿入されました')
     },
   )
+})
+
+app.post('/close', (req: express.Request, res: express.Response) => {
+  if (!req.body.id && typeof req.body.close !== 'number') {
+    res.status(400).json({ message: 'No User ID' })
+    return
+  }
+
+  if (!req.body.close && typeof req.body.close !== 'boolean') {
+    res.status(400).json({ message: 'No User Close' })
+    return
+  }
+
+  const id: number = req.body.id
+  const close: boolean = req.body.close
+
+  db.all('SELECT * FROM users WHERE userId = (?)', [id], (err, rows) => {
+    if (err || rows.length < 1) {
+      console.error(err)
+      res.status(400).json({
+        message: 'Invailed User ID',
+      })
+    }
+
+    db.run(
+      'INSERT INTO close_table (userId, close) VALUES (?, ?) ON CONFLICT(userId) DO UPDATE SET close=excluded.close;',
+      [id, close ? 1 : 0],
+      (err) => {
+        if (err) {
+          console.error(err)
+          res.status(500).json({ message: 'Failed to update close status' })
+          return
+        }
+      },
+    )
+  })
+  res.status(200).send()
+  return
 })
 
 app.listen(port, () => {
